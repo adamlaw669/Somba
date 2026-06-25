04877e5e47d7285e890b0d8124c395d2a6a25b92
Adam air <adamlawal669@gmail.com>
2026-06-25 11:50:25 +0100

    stub

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+
 from fastapi import FastAPI, Request
 
 app = FastAPI()
+
 
 @app.post("/webhooks/nomba")
 async def nomba_webhook(request: Request):
@@ -8,6 +11,13 @@ async def nomba_webhook(request: Request):
     print("Nomba event received:", event)
     return {"status": "received"}
 
+
 @app.get("/")
 async def health():
     return {"status": "Somba webhook is aliveee"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
