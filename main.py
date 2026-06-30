a4488dd3fc681287f2642c5c49bda0ee0c046aee
Adam air <adamlawal669@gmail.com>
2026-06-30 12:16:17 +0100

    chore: fixed an indent error in main

--- main.py
+++ main.py
@@ -10,8 +10,5 @@ from somba.api.app import app
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    import uvicorn
-=======
->>>>>>> refs/remotes/origin/main
+    
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
